f4f65b04fbc82ad2fb4558c11b89bed34daa2d0b
progressivewapps <dumapathi.mcg@gmail.com>
2018-08-21 23:00:35 +0530

    activate code

--- sw.js
+++ sw.js
@@ -4,4 +4,5 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("activate", function(event) {
     console.log('[service worker] activating service worker..', event);
+    return self.clients.claim();
 });
