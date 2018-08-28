47612d3087929a38a1d69bc8459dc89430d72081
progressivewapps <dumapathi.mcg@gmail.com>
2018-08-28 23:02:12 +0530

    add buttons updated

--- js/feed.js
+++ js/feed.js
@@ -3,4 +3,22 @@ var installButton = document.querySelector("#add_installBtn");
 installButton.addEventListener("click", function(event){
     event.preventDefault();
     console.log("BeforeInstall prompt activated");
+    openCreateModal();
 });
+
+function openCreateModal() {
+    if(defferedPrompt) {
+        defferedPrompt.prompt();
+
+        defferedPrompt.userChoice.then(function(choiceResult) {
+            console.log("choice is: " + choiceResult);
+            if(choiceResult.outcome == 'dismissed') {
+                console.log('User cancelled the installed');
+            }
+            else {
+                console.log('User add to Home screen');
+            }
+        });
+        defferedPrompt = null;
+    }
+}
